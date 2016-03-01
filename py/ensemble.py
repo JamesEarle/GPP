8d004c6576d647c5c102ada81ee394e9aef4e6e7
James Earle <j_earle@hotmail.com>
2016-02-29 23:57:06 -0500

    Improved Python stats calculations. Outlier detection using 2-sigma rule

--- py/ensemble.py
+++ py/ensemble.py
@@ -1,7 +1,9 @@
 import os
 import sys
 import active
+import statistics as stat
 from os import listdir
+
 
 ## Author: James Earle
 ##
@@ -9,7 +11,7 @@ from os import listdir
 ## "opinions" as to the solution of a problem. In this sense, we 
 ## save 20 best-of-run individuals and average their predictions
 ## regarding the future. This provides us with a range of interpretation.
-
+    
 activeDirectory = active.listActiveDirectory()
 files = listdir(activeDirectory)
 
@@ -28,36 +30,74 @@ for x in range(0, 20):
 
 std_dev_values = []
 avg_values = []
+med_values = []
 min_values = []
 max_values = []
 
+# Do aggregation for however many data points we have.
 for x in range(0, len(inds[0])):
-    average = 0
-    max = -sys.maxsize - 1
-    min = sys.maxsize
+    # average = 0
+    # max = -sys.maxsize - 1
+    # min = sys.maxsize
     
     # Don't need min or max, instead we create a list of the 20 entries.
     # values_at_index = []
     
+    temp = []
     # Iterate 20 individuals at a single index (Column Major Order)
     for y in range(0, 20):
         val = float(inds[y][x])
-        average += val
-        max = (val if val > max else max)
-        min = (val if val < min else min)
+        temp.append(val)
+        # average += val
+        # max = (val if val > max else max)
+        # min = (val if val < min else min)
         # values_at_index.append(val)
-        
+    
+    # print(sorted(temp))
+    
+    ind = 0
+    temp = sorted(temp)
+    
+    # Find the min that is within 2-sigma
+    while True:
+        val = temp[ind]
+        if(abs(val - stat.mean(temp)) > (2*stat.stdev(temp))):
+            del temp[ind]
+            ind += 1
+        else:
+            min_values.append(temp[ind])
+            break
+    
+    ind = 0
+    temp = sorted(temp, reverse=True) # Reverse
+    
+    # Find the max that is within 2-sigma
+    while True:
+        val = temp[ind]
+        if(abs(val - stat.mean(temp)) > (2*stat.stdev(temp))):
+            del temp[ind]
+            ind += 1
+        else:
+            max_values.append(temp[ind])
+            break
+            
+    std_dev_values.append(stat.stdev(temp))
+    avg_values.append(stat.mean(temp))
+    med_values.append(stat.median(temp))
+    # min_values.append(min(temp))
+    # max_values.append(max(temp))
+    
     # Average is currently a sum, divide by number of runs
-    average /= 20
+    # average /= 20
     
     # We have to reiterate the list. Can't calculate Std Dev without average
-    sum = 0
-    for y in range(0, 20):
-        value = float(inds[y][x])
-        sum += (value - average)**2
+    # sum = 0
+    # for y in range(0, 20):
+        # value = float(inds[y][x])
+        # sum += (value - average)**2
     
     # Std. Dev. is the sum divided out by 20 runs
-    sum /= 20
+    # sum /= 20
     
     # print(values_at_index)
     # values_at_index = sorted(values_at_index)
@@ -81,10 +121,10 @@ for x in range(0, len(inds[0])):
     # print("+" + str(max))
     # print(values_at_index)
     
-    std_dev_values.append(sum)
-    avg_values.append(average)
-    min_values.append(min)
-    max_values.append(max)
+    # std_dev_values.append(sum)
+    # avg_values.append(average)
+    # min_values.append(min)
+    # max_values.append(max)
         
 with \
 open(activeDirectory + "/ensemble-avg.txt", 'w') as avg, \
