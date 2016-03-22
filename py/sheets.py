89fedf8490892588f05a994b6ee744be0cc7be2b
James Earle <j_earle@hotmail.com>
2016-03-21 21:27:38 -0400

    automated ensemble data formatting in spreadsheets with sheets.py using openpyxl

--- py/sheets.py
+++ py/sheets.py
@@ -4,18 +4,22 @@ import datetime
 from os import listdir
 from openpyxl import Workbook
 
-# Create spreadsheets with data filled in so I only have to select data and graph it.
+def ensembleToSpreadsheet(filename, col, offset, ws):
+    arr = []
+    for line in open(filename, 'r'):
+        arr.append(float(line))
+    for x in range(0, len(arr)):
+        ws[col + str(x + offset)] = arr[x]
 
+# Create spreadsheets with data filled in so I only have to select data and graph it.
 wb = Workbook()
 ws = wb.active
 
 ws['A1'] = "DJIA"
-ws['B1'] = "BEST_TRAIN"
-ws['C1'] = "BEST_TEST"
-ws['D1'] = "ENS_AVG"
-ws['E1'] = "ENS_MED"
-ws['F1'] = "ENS_MIN"
-ws['G1'] = "ENS_MAX"
+ws['B1'] = "ENS_AVG"
+ws['C1'] = "ENS_MED"
+ws['D1'] = "ENS_MIN"
+ws['E1'] = "ENS_MAX"
 
 activeDirectory = active.listActiveDirectory()
 files = listdir(activeDirectory)
@@ -23,11 +27,23 @@ files = listdir(activeDirectory)
 input = "D:/Development/GitHub/GPP/GeneticProgrammingPortfolio/input/DJIA/djia_close_12-16.txt"
 input_arr = []
 
+# Read and write Dow Jones Index
 for line in open(input, 'r'):
     input_arr.append(float(line))
-
 for x in range(0, len(input_arr)):
     ws['A' + str(x+2)] = input_arr[x]
+
+# avg = []
+# for line in open(activeDirectory + "/ensemble-avg.txt", "r"):
+#     avg.append(float(line))
+# for x in range(0, len(avg)):
+#     ws['B' + str(x+698)] = avg[x]
+
+ensembleToSpreadsheet(activeDirectory + "/ensemble-avg.txt", 'B', 698, ws)
+ensembleToSpreadsheet(activeDirectory + "/ensemble-med.txt", 'C', 698, ws)
+ensembleToSpreadsheet(activeDirectory + "/ensemble-min.txt", 'D', 698, ws)
+ensembleToSpreadsheet(activeDirectory + "/ensemble-max.txt", 'E', 698, ws)
+
 
 # Save the file
 wb.save(activeDirectory + "/sample.xlsx")
